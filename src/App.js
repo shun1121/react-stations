dac4da7161b87131028c7091ec6fa369ef274702
shun1121 <hikitanikun.ware80000@gmail.com>
2022-02-02 22:56:42 +0900

    added css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // DO NOT DELETE
 
-import * as React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 
 /**
@@ -8,9 +8,22 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  console.log("aaaaaaa")
+  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg')
+  const url = 'https://dog.ceo/api/breeds/image/random'
+  const handleClick = async () => {
+    await fetch(url)
+    .then(res => res.json())
+    .then(data => setDogUrl(data.message))
+  }
   return (
-    <div>
-      <h2>Hello, world!</h2>
+    <div className='wrapper'>
+      <header className='title'>dogアプリ</header>
+      <p className='description'>犬の画像を表示するアプリです。</p>
+      <button className='button' onClick={handleClick}>おせおせ</button>
+      <div className='imgWrapper'>
+        <img src={dogUrl} />
+      </div>
     </div>
   )
 }
