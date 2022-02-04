2877302b6d08709d8f5d926dbb39d65045824ae0
shun1121 <hikitanikun.ware80000@gmail.com>
2022-02-05 02:28:06 +0900

    use useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // DO NOT DELETE
 
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { Header } from './Header'
 import { Description } from './Description'
 import './App.css'
@@ -11,11 +11,11 @@ import './App.css'
  */
 export const App = () => {
   const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg')
-  const url = 'https://dog.ceo/api/breeds/image/random'
+  const url = 'https://dog.ceo/api/breeds/list/all'
   const handleClick = async () => {
     await fetch(url)
     .then(res => res.json())
-    .then(data => setDogUrl(data.message))
+    .then(data => console.log(data.message))
   }
   return (
     <div className='wrapper'>
--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -1,1 +1,13 @@
 // DO NOT DELETE
+import { useState, useEffect } from 'react'
+
+export const DogListContainer = () => {
+  const [breeds, setBreeds] = useState({})
+  
+  useEffect(() => {
+    const url = 'https://dog.ceo/api/breeds/list/all' 
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setBreeds(data.message))
+  },[])
+}
