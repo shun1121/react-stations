a9b477ebef4fcfa361c3d830771364128e1c0d23
shun1121 <hikitanikun.ware80000@gmail.com>
2022-02-06 02:00:48 +0900

    use selectbox

--- src/BreedsSelect.js
+++ src/BreedsSelect.js
@@ -2,17 +2,12 @@
 import React from 'react'
 
 export const BreedsSelect = ({list, val, handleChange}) => {
-  // console.log(props)
-  // const breedlis = list.map((breedlist, i) => (
-  //   <option key={i} value={breedlist}>{breedlist}</option>
-  // ))
-  // console.log(breedlis)
   const breedlists = list
 
   return (
     <select value={val} onChange={handleChange} >
       <option>選択してください</option>
-      {breedlists.map((breedlist, i) => (
+      {(breedlists || []).map((breedlist, i) => (
         <option key={i} value={breedlist}>{breedlist}</option>
       ))}
     </select>
