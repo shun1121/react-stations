a5ea4d8dfe4691f2dc7b0226f962b124ba6e45d3
shun1121 <hikitanikun.ware80000@gmail.com>
2022-02-06 01:54:22 +0900

    use selectbox

--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -3,14 +3,20 @@ import React, { useState, useEffect } from 'react'
 import { BreedsSelect } from './BreedsSelect'
 
 export const DogListContainer = () => {
-  const [breeds, setBreeds] = useState(['initial'])
+  const [breeds, setBreeds] = useState(['initial', 'state'])
   const [selectedBreed, setSelectedBreed] = useState('')
+  const [loading, setLoading] = useState(false);
+
   
   useEffect(() => {
+    setLoading(true);
     const url = 'https://dog.ceo/api/breeds/list/all' 
     fetch(url)
     .then(res => res.json())
     .then(data => setBreeds(Object.keys(data.message)))
+    .finally(() => {
+      setLoading(false);
+    });
   },[])
 
   const handleChange = (e) => {
@@ -19,6 +25,11 @@ export const DogListContainer = () => {
 
   console.log(selectedBreed)
   return (
-    <BreedsSelect list={breeds} val={selectedBreed} handleChange={handleChange} />
+    <>
+      {loading
+        ? <p>Data is loading...</p>
+        : <BreedsSelect list={breeds} val={selectedBreed} handleChange={handleChange} />
+      }
+    </>
   )
 }
