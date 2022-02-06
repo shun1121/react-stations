01cad668aa3e44e2d71c742c4703eb4b9d52cd44
shun1121 <hikitanikun.ware80000@gmail.com>
2022-02-06 19:19:52 +0900

    show dogs images

--- src/DogListContainer.js
+++ src/DogListContainer.js
@@ -5,6 +5,7 @@ import { BreedsSelect } from './BreedsSelect'
 export const DogListContainer = () => {
   const [breeds, setBreeds] = useState(['initial', 'state'])
   const [selectedBreed, setSelectedBreed] = useState('')
+  const [breedLists, setBreedLists] = useState([])
   const [loading, setLoading] = useState(false);
 
   
@@ -23,13 +24,30 @@ export const DogListContainer = () => {
     setSelectedBreed(e.target.value)
   }
 
-  console.log(selectedBreed)
+  const handleClick = () => {
+    if (selectedBreed) {
+      console.log(selectedBreed)
+      const url = `https://dog.ceo/api/breed/${selectedBreed}/images/random/3`
+      fetch(url)
+      .then(res => res.json())
+      .then(data => setBreedLists(data.message))
+    }
+  }
+
+  console.log(breedLists)
   return (
     <>
       {loading
         ? <p>Data is loading...</p>
-        : <BreedsSelect list={breeds} val={selectedBreed} handleChange={handleChange} />
+        : 
+        <>
+          <BreedsSelect list={breeds} val={selectedBreed} handleChange={handleChange} />
+          <button onClick={handleClick}>表示</button>
+        </>
       }
+      {(breedLists || []).map((breedList, i) => (
+        <img key={i} src={breedList} />
+      ))}
     </>
   )
 }
